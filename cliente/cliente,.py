9ea2caec0d842705de4023d273b33c50a6eec7ab
GustavoDiego <gustavodiego298@gmail.com>
2024-01-30 13:59:28 -0300

    Cliente
    Foi adicionado novos comentários ao código para melhor entendimento.

--- cliente/cliente,.py
+++ cliente/cliente,.py
@@ -4,42 +4,49 @@ import threading
 from datetime import datetime
 import os
 
+# Configuração do cliente UDP
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client.bind(("localhost", random.randint(8000, 9998)))
 
+# Solicita ao usuário que insira um nome para se conectar à sala
 nome = input("Digite seu nome para se conectar a sala: ")
 
+# Função para receber mensagens do servidor
 def receber():
     lista_de_partes =[]
     while True:
         try:
+            # Recebe mensagens do servidor
             mensagem, _ = client.recvfrom(1024)
+
+            # Verifica se a mensagem é um indicador de início ou fim de arquivo
             if mensagem.decode() == '!1!1!' or mensagem.decode() == '!0!0!':
                 card = ''
                 nome_juntado = f"{datetime.now().strftime('%Y%m%d%H%M%S')}.txt"
+
+                # Recebe partes do arquivo até encontrar o indicador de fim
                 while True:
-
-
                     mensagem, _ = client.recvfrom(1024)
                     if mensagem.decode() == '!0!0!':
                         break
                     elif mensagem.decode() == '!1!1!':
                         continue
                     else:
-                        card+=mensagem.decode()
+                        card += mensagem.decode()
 
+                # Salva o conteúdo do arquivo em um arquivo local
                 with open(nome_juntado,'w') as file:
                     file.write(card)
-                with open (nome_juntado,'r') as file:
+                with open(nome_juntado,'r') as file:
                     ult = file.read()
                     print(ult)
 
-
+            # Processa mensagens regulares
             if mensagem.decode() != "!0!0!":
                 nome_arquivo = f"{datetime.now().strftime('%Y%m%d%H%M%S')}.txt"
                 data_hora = f"{datetime.now().strftime('%H:%M:%S - %d/%m/%Y')}"
 
-                # Abre o arquivo para escrita
+                # Salva a mensagem em um arquivo local
                 with open(nome_arquivo, "w") as file:
                     file.write(f'{mensagem.decode()}\n')
 
@@ -51,10 +58,14 @@ def receber():
                 print(f'{ip}:{porta}/~{nome}: {arquivo_final} {data_hora}')
         except:
             pass
+
+# Inicia uma thread para receber mensagens
 t = threading.Thread(target=receber)
 t.start()
 
+# Loop principal para o envio de mensagens
 while True:
+    # Se o nome começar com "hi, meu nome eh:", remove essa parte
     if "hi, meu nome eh:" in nome:
         nome = nome[15:]
         while True:
@@ -66,41 +77,33 @@ while True:
                 # Gera o nome do arquivo usando a data atual e o nome do autor
                 nome_arquivo = f"{datetime.now().strftime('%Y%m%d%H%M%S')}.txt"
 
-                # Abre o arquivo para escrita
+                # Salva a mensagem em um arquivo local
                 with open(nome_arquivo, "w") as file:
                     file.write(f'{mensagem}\n')
 
+                # Verifica se o arquivo é grande demais para ser enviado em uma única mensagem
                 tamanho_max = 800
                 tamanho_do_arquivo = os.path.getsize(nome_arquivo)
 
-
                 if tamanho_do_arquivo >= tamanho_max:
-                    with open(nome_arquivo,"rb") as file:
-
+                    # Envia o arquivo em partes
+                    with open(nome_arquivo, "rb") as file:
                         while True:
-
                             parte_do_arquivo = file.read(1024)
-
                             if not parte_do_arquivo:
                                 flag = '!0!0!'
-                                client.sendto(flag.encode(),("localhost",5555))
+                                client.sendto(flag.encode(),("localhost", 5555))
                                 break
 
                             flag = '!1!1!'
                             client.sendto(flag.encode(),("localhost", 5555))
-
                             client.sendto(parte_do_arquivo,("localhost", 5555))
-
-
                 else:
-                    # Lê o conteúdo do arquivo
+                    # Envia o arquivo como uma única mensagem
                     with open(nome_arquivo, "rb") as file:
                         arquivo_bytes = file.read()
 
-
-                    # Envia o arquivo para o servidor
-
                     client.sendto(arquivo_bytes, ("localhost", 5555))
-
     else:
+        # Solicita ao usuário que insira um nome para se conectar à sala
         nome = input("Digite seu nome para se conectar a sala: ")
