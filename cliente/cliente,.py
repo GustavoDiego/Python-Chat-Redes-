c93cb7f7bf7a33326b468afd2e62b5a7a51de714
GustavoDiego <gustavodiego298@gmail.com>
2024-01-30 13:46:07 -0300

    Atualização Cliente
    Nessa fase foi adicionado, thread para que partes do código funcionem de maneira autônoma. Além da adição de espaço para entrada e saídas com suas devidas condições e casos.

--- cliente/cliente,.py
+++ cliente/cliente,.py
@@ -51,3 +51,56 @@ def receber():
                 print(f'{ip}:{porta}/~{nome}: {arquivo_final} {data_hora}')
         except:
             pass
+t = threading.Thread(target=receber)
+t.start()
+
+while True:
+    if "hi, meu nome eh:" in nome:
+        nome = nome[15:]
+        while True:
+            mensagem = input('Digite sua mensagem (ou "bye" para sair): ')
+            if mensagem == "bye":
+                print(f'{nome} deixou o servidor :(')
+                exit()
+            else:
+                # Gera o nome do arquivo usando a data atual e o nome do autor
+                nome_arquivo = f"{datetime.now().strftime('%Y%m%d%H%M%S')}.txt"
+
+                # Abre o arquivo para escrita
+                with open(nome_arquivo, "w") as file:
+                    file.write(f'{mensagem}\n')
+
+                tamanho_max = 800
+                tamanho_do_arquivo = os.path.getsize(nome_arquivo)
+
+
+                if tamanho_do_arquivo >= tamanho_max:
+                    with open(nome_arquivo,"rb") as file:
+
+                        while True:
+
+                            parte_do_arquivo = file.read(1024)
+
+                            if not parte_do_arquivo:
+                                flag = '!0!0!'
+                                client.sendto(flag.encode(),("localhost",5555))
+                                break
+
+                            flag = '!1!1!'
+                            client.sendto(flag.encode(),("localhost", 5555))
+
+                            client.sendto(parte_do_arquivo,("localhost", 5555))
+
+
+                else:
+                    # Lê o conteúdo do arquivo
+                    with open(nome_arquivo, "rb") as file:
+                        arquivo_bytes = file.read()
+
+
+                    # Envia o arquivo para o servidor
+
+                    client.sendto(arquivo_bytes, ("localhost", 5555))
+
+    else:
+        nome = input("Digite seu nome para se conectar a sala: ")
