66bbe46f629a8cb6365580261a7028a305e00d0b
GustavoDiego <gustavodiego298@gmail.com>
2024-02-27 11:58:57 -0300

    implementação do checksum
    ainda não testada, apenas para atualização de código.

--- cliente/cliente.py
+++ cliente/cliente.py
@@ -3,11 +3,36 @@ import random
 import threading
 from datetime import datetime
 import os
+import struct
 
 # Configuração do cliente UDP
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 p = random.randint(8000, 9998)
-client.bind(("localhost", p))
+client.bind(("localhost", p)) #alterar por ser desnecessário
+unpacker = struct.Struct('5s H 800s')
+packer = struct.Struct('5s H 800s')
+
+
+def __int_chksum(byte_msg):
+
+    total = 0
+    length = len(byte_msg)  # length of the byte message object
+    i = 0
+    while length > 1:
+        total += ((byte_msg[i + 1] << 8) & 0xFF00) + ((byte_msg[i]) & 0xFF)
+        i += 2
+        length -= 2
+
+    if length > 0:
+        total += (byte_msg[i] & 0xFF)
+
+    while (total >> 16) > 0:
+        total = (total & 0xFFFF) + (total >> 16)
+
+    total = ~total
+
+    return total & 0xFFFF
+
 
 # Solicita ao usuário que insira um nome para se conectar à sala
 nome = input("Digite seu nome para se conectar a sala: ")
@@ -20,18 +45,23 @@ def receber():
         try:
             # Recebe mensagens do servidor
             mensagem, _ = client.recvfrom(1024)
+            flag, checksum, mensagem = packer.pack(mensagem)
+            mensagem = mensagem.decode().rstrip('\x00')
 
             # Verifica se a mensagem é um indicador de início ou fim de arquivo
-            if mensagem.decode() == '!1!1!' or mensagem.decode() == '!0!0!':
+            if flag.decode() == '!1!1!':
                 card = ''
                 nome_juntado = f"{datetime.now().strftime('%Y%m%d%H%M%S')}.txt"
 
                 # Recebe partes do arquivo até encontrar o indicador de fim
                 while True:
                     mensagem, _ = client.recvfrom(1024)
-                    if mensagem.decode() == '!0!0!':
+                    flag, checksum, mensagem = packer.pack(mensagem)
+                    mensagem = mensagem.decode().rstrip('\x00')
+
+                    if flag.decode() == '!0!0!':
                         break
-                    elif mensagem.decode() == '!1!1!':
+                    elif flag.decode() == '!1!1!':
                         continue
                     else:
                         card += mensagem.decode()
@@ -44,7 +74,7 @@ def receber():
                     print(f'{ip}:{p}/~ {arquivo_final} {data_hora}')
 
             # Processa mensagens regulares
-            if mensagem.decode() != "!0!0!":
+            else:
                 nome_arquivo = f"{datetime.now().strftime('%Y%m%d%H%M%S')}.txt"
                 data_hora = f"{datetime.now().strftime('%H:%M:%S - %d/%m/%Y')}"
 
@@ -55,7 +85,7 @@ def receber():
                 # Lê o conteúdo do arquivo
                 with open(nome_arquivo, "r") as file:
                     arquivo_final = file.read()
-                ip = _[0]
+                ip = _[0] #alterar nome do _
 
 
                 if c:
@@ -97,26 +127,38 @@ while True:
                 # Verifica se o arquivo é grande demais para ser enviado em uma única mensagem
                 tamanho_max = 800
                 tamanho_do_arquivo = os.path.getsize(nome_arquivo)
-
+                
+                flag = b'!0!0!'
                 if tamanho_do_arquivo >= tamanho_max:
                     # Envia o arquivo em partes
                     with open(nome_arquivo, "rb") as file:
                         while True:
                             parte_do_arquivo = file.read(1024)
                             if not parte_do_arquivo:
-                                flag = '!0!0!'
-                                client.sendto(flag.encode(),("localhost", 5555))
+                                flag = b'!0!0!'
+                                checksum = 0
+                                msg = 'fim'
+                                ini_msg = packer.pack(flag,checksum, msg)
+                                checksum = __int_chksum(bytearray(ini_msg))
+                                fin_msg = packer.pack(flag,checksum, msg)
+                                client.sendto(fin_msg,("localhost", 5555))
                                 break
 
-                            flag = '!1!1!'
-                            client.sendto(flag.encode(),("localhost", 5555))
-                            client.sendto(parte_do_arquivo,("localhost", 5555))
+                            flag = b'!1!1!'
+                            checksum = 0
+                            ini_msg = packer.pack(flag,checksum, parte_do_arquivo)
+                            checksum = __int_chksum(bytearray(ini_msg)) #bytearray não sei se vai ser necessário
+                            fin_msg = packer.pack(flag, checksum, parte_do_arquivo)
+                            client.sendto(fin_msg,("localhost", 5555))
                 else:
                     # Envia o arquivo como uma única mensagem
                     with open(nome_arquivo, "rb") as file:
                         arquivo_bytes = file.read()
-
-                    client.sendto(arquivo_bytes, ("localhost", 5555))
+                    checksum = 0
+                    ini_msg = packer.pack(flag,checksum, arquivo_bytes)
+                    checksum = __int_chksum(bytearray(ini_msg))
+                    fin_msg = packer.pack(flag,checksum, arquivo_bytes)
+                    client.sendto(fin_msg, ("localhost", 5555))
     else:
         # Solicita ao usuário que insira um nome para se conectar à sala
         nome = input("Digite seu nome para se conectar a sala (digite 'hi, meu nome eh: >nome<'): ")
